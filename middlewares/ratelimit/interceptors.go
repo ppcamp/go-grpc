dda08a2c200b4a9f6eb7d26aaf41c3ecbfc7948e
ppcamp <p.augustocampos@gmail.com>
2022-07-26 14:12:54 -0300

    fix: atomicity issue in counting variable of ratelimit pkg

--- middlewares/ratelimit/interceptors.go
+++ middlewares/ratelimit/interceptors.go
@@ -2,26 +2,28 @@ package ratelimit
 
 import (
 	"context"
+	"sync/atomic"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-type serverClose interface {
-	GracefulStop()
-}
+type serverClose interface{ GracefulStop() }
 
-func UnaryInterceptor(n int) grpc.UnaryServerInterceptor {
-	c := 0
+// UnaryInterceptor implements a gRPC middleware that allows n requests before starts rejecting.
+// The interceptor is threadsafe (uses atomic operations).
+func UnaryInterceptor(n uint64) grpc.UnaryServerInterceptor {
+	c := uint64(0)
+
 	return func(
 		ctx context.Context,
 		req interface{},
 		info *grpc.UnaryServerInfo,
 		next grpc.UnaryHandler,
 	) (resp interface{}, err error) {
-		c++
-		if c == n {
+		atomic.AddUint64(&c, 1)
+		if atomic.LoadUint64(&c) == n {
 			s, ok := info.Server.(serverClose)
 			if !ok {
 				panic("fail to get the server")
@@ -34,16 +36,19 @@ func UnaryInterceptor(n int) grpc.UnaryServerInterceptor {
 	}
 }
 
-func StreamInterceptor(n int) grpc.StreamServerInterceptor {
-	c := 0
+// UnaryInterceptor implements a gRPC middleware that allows n requests before starts rejecting.
+// The interceptor is threadsafe (uses atomic operations).
+func StreamInterceptor(n uint64) grpc.StreamServerInterceptor {
+	c := uint64(0)
+
 	return func(
 		srv interface{},
 		ss grpc.ServerStream,
 		info *grpc.StreamServerInfo,
 		next grpc.StreamHandler,
 	) error {
-		c++
-		if c == n {
+		atomic.AddUint64(&c, 1)
+		if atomic.LoadUint64(&c) == n {
 			s, ok := srv.(serverClose)
 			if !ok {
 				panic("fail to get the server")
